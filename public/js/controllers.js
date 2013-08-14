55e425ccc10c48a6e9bf86d1c7b0f653eff12490
Daniel Chabr <danielchabr@gmail.com>
2013-08-14 14:21:22 +0200

    Fronted hashing of password added

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,17 +1,26 @@
 //angular.module('main.service' []);
 
 //angular.module('main', ['main.service']);
-
+/*
+		<script>
+			var hash = CryptoJS.SHA3("Message", { outputLength: 512 });
+			var hash = CryptoJS.SHA3("Message", { outputLength: 384 });
+			var hash = CryptoJS.SHA3("Message", { outputLength: 256 });
+			var hash = CryptoJS.SHA3("Message", { outputLength: 224 });
+		</script>
+		*/
 
 function mainControl($scope, $http) {
 	$scope.signup = function() {
-		$http.post('/signup', {email: $scope.signup_email, password: $scope.signup_password}).success( function(data) {
-			$scope.message = data;
+		hash = CryptoJS.SHA3($scope.signup_password + $scope.signup_email, {outputLength: 256 });
+		$http.post('/signup', {'email': $scope.signup_email, 'password': hash.toString()}).success( function(data) {
+			$scope.message = data + " " + hash;
 		});
 	}
 	$scope.login = function() {
-		$http.post('/login', {email: $scope.login_email, password: $scope.login_password}).success( function(data) {
-			$scope.message = data;
+		hash = CryptoJS.SHA3($scope.login_password + $scope.login_email, {outputLength: 256 });
+		$http.post('/login', {'email': $scope.login_email, 'password': hash.toString()}).success( function(data) {
+			$scope.message = data + " " + hash;
 		});
 	}
 
