831bff664e2296c431a864e08bf18716d19c1b59
Daniel Chabr <danielchabr@gmail.com>
2013-08-14 12:48:06 +0200

    Simple registration and login implemented with database

--- app.js
+++ app.js
@@ -2,36 +2,69 @@ var fs = require('fs');
 var express = require('express');
 var mongoose = require('mongoose');
 
-var homepage = fs.readFileSync('./public/index.html', 'utf-8');
+mongoose.connect('mongodb://localhost/users');
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+	email:{ type:String, required:true, unique:true },
+	password:{ type:String, required:true}
+});
+var User = mongoose.model('UserModel', userSchema);
+//   var me = new User();
+//   me.save();
+ //  User.find({}, function (err, docs) {
+  // console.log(docs);
+   //});
+
 
 var app = express();
 app.use(express.compress());
 app.use(express.bodyParser());
+
 app.use(express.static(__dirname + '/public'));
 app.post('/signup', function(request, response) {
-	console.log(request.body.email);
-
-	response.send("Welcome "+request.body.email);
+		User.findOne({email:request.body.email}, function (err, users) {
+			if(err) {console.log(err)};
+			if(!users) {
+				var user = new User();
+				user.email = request.body.email;
+				user.password = request.body.password;
+				user.save();
+				response.cookie('user', request.body.email, { maxAge: 900000, httpOnly: true });
+				response.send('registered');
+				console.log('registered');
+			}
+			else {
+				response.send('emailExists');
+				console.log('emailExists');
+			}
+		});
 });
-//app.use(function (req, res) {
-//	res.redirect('/');
-//});
-
-/*
-mongoose.connect('mongodb://localhost/users');
-var Schema = mongoose.Schema;
-var userSchema = new Schema({
-		username:{ type:String, required:true, unique:true },
-		email:{ type:String, required:false, unique:false },
-		password:{ type:String },//required:true}
+app.post('/login', function(request, response) {
+		User.findOne({email:request.body.email}, function (err, users) {
+			if(err) {console.log(err)};
+			if(!users) {
+				response.send('loginFailure');
+				console.log('loginFailure');
+			}
+			else {
+				if(users.password == request.body.password){
+					response.cookie('user', request.body.email, { maxAge: 900000, httpOnly: true });
+					response.send('loginSuccess');
+					console.log('loginSuccess');
+				}
+				else {
+					response.send('loginFailure');
+					console.log('loginFailure');
+				}
+			}
+		});
 });
-var User = mongoose.model('UserModel', userSchema);
-var me = new User();
-me.username = 'morgus';
-me.save();
-User.find({}, function (err, docs) {
-	console.log(docs);
+app.use(function (req, res) {
+	res.redirect('/');
 });
-*/
 
 app.listen(process.env.PORT || 8080);
+
+/*
+   */
+
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,7 +5,12 @@
 
 function mainControl($scope, $http) {
 	$scope.signup = function() {
-		$http.post('/signup', {email: $scope.email, password: $scope.password}).success( function(data) {
+		$http.post('/signup', {email: $scope.signup_email, password: $scope.signup_password}).success( function(data) {
+			$scope.message = data;
+		});
+	}
+	$scope.login = function() {
+		$http.post('/login', {email: $scope.login_email, password: $scope.login_password}).success( function(data) {
 			$scope.message = data;
 		});
 	}
