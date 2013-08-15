f8fd424357521bdca4b83b762654bc5e9d89836f
Daniel Chabr <danielchabr@gmail.com>
2013-08-15 15:51:25 +0200

    Private site up, authentication seems to work, needs more testing

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,6 @@ angular.module('mainapp', []).
 		$routeProvider
 		.when('/', {templateUrl: '/partials/home.html',   controller: homeControl})
 		.when('/login', {templateUrl: '/partials/welcome.html',   controller: welcomeControl})
-		.otherwise({redirectTo: '/login'});
+		.otherwise({redirectTo: '/'});
 	$locationProvider.html5Mode(true);
 }]);
