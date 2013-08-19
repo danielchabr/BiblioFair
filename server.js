c1f657eb61df35f7a7f02e0b382944771d50efc0
Daniel Chabr <danielchabr@gmail.com>
2013-08-19 12:56:11 +0200

    Token authentication and minor bugs

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = require('./lib/router');
-var usersdb = require('./lib/usersdb');
+var db = require('./lib/db');
 
 var app = express();
-usersdb.connect();
+db.connect();
 
-router(app, usersdb);
+router(app, db);
 
 var port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;  
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
