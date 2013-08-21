bd2bebfb86373af69e167700ea04210c24a4f66c
Daniel Chabr <danielchabr@gmail.com>
2013-08-21 20:21:27 +0200

    Renamed to BiblioFair

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,15 +1,3 @@
-//angular.module('main.service' []);
-
-//angular.module('main', ['main.service']);
-/*
-		<script>
-			var hash = CryptoJS.SHA3("Message", { outputLength: 512 });
-			var hash = CryptoJS.SHA3("Message", { outputLength: 384 });
-			var hash = CryptoJS.SHA3("Message", { outputLength: 256 });
-			var hash = CryptoJS.SHA3("Message", { outputLength: 224 });
-		</script>
-		*/
-
 function loginControl($rootScope, $scope, $http, $location) {
 	$scope.signup = function() {
 		hash = CryptoJS.SHA3($scope.signup_password + $scope.signup_email, {outputLength: 256 });
