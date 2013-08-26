f4440b0c7ffe9d78c1648607e1fa38fd0efda592
Daniel Chabr <danielchabr@gmail.com>
2013-08-26 22:22:04 +0200

    Tried to repair triggers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,4 @@
 function loginControl($rootScope, $scope, $http, $location) {
-	$('#div_signup_email').tooltip({placement: 'right', trigger: 'manual'});
-	$('#div_signup_password').tooltip({placement: 'right', trigger: 'manual'});
 	$scope.signup = function() {
 		if($scope.signup_email && $scope.signup_password.length > 5) {
 			hash = CryptoJS.SHA3($scope.signup_password + $scope.signup_email, {outputLength: 256 });
@@ -9,15 +7,7 @@ function loginControl($rootScope, $scope, $http, $location) {
 					$scope.message = data;
 				});
 		}
-		else {
-			if(!$scope.signup_email) $('#div_signup_email').tooltip('show');
-			else $('#div_signup_email').tooltip('hide');
-			if($scope.signup_password.length < 6) $('#div_signup_password').tooltip('show');
-			else $('#div_signup_password').tooltip('hide');
-		}
 	}
-	$('#div_login_email').tooltip({placement: 'right', trigger: 'manual'});
-	$('#div_login_password').tooltip({placement: 'right', trigger: 'manual'});
 	$scope.login = function() {
 		if($scope.login_email && $scope.login_password.length > 5) {
 			hash = CryptoJS.SHA3($scope.login_password + $scope.login_email, {outputLength: 256 });
@@ -36,14 +26,7 @@ function loginControl($rootScope, $scope, $http, $location) {
 				$scope.message = "Pleasy try again";
 			});
 		}
-		else {
-			if(!$scope.login_email) $('#div_login_email').tooltip('show');
-			else $('#div_login_email').tooltip('hide');
-			if($scope.login_password.length < 6) $('#div_login_password').tooltip('show');
-			else $('#div_login_password').tooltip('hide');
-		}
 	}
-
 }
 function homeControl($rootScope, $scope, $http, $location) {
 	$scope.bookOrder = 'title';
