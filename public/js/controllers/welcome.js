7ab32c10cc234a780e7b0d11fe9998fb7c5c08af
Daniel Chabr <danielchabr@gmail.com>
2013-09-09 21:54:05 +0200

    Username added

--- public/js/controllers/welcome.js
+++ public/js/controllers/welcome.js
@@ -1,15 +1,18 @@
 function welcomeControl($rootScope, $scope, $http, $location, $translate) {
-	$scope.log = function (l) { console.log(l);};
 	$scope.signup = function() {
-		if($scope.signup_email && $scope.signup_password.length > 5) {
-			hash = CryptoJS.SHA3($scope.signup_password + $scope.signup_email, {outputLength: 256 });
-			$http.post('/signup', {'email': $scope.signup_email, 'password': hash.toString()})
+		if($scope.signup_email && $scope.signup_username && $scope.signup_password.length > 5) {
+			hash_username = CryptoJS.SHA3($scope.signup_password + $scope.signup_username, {outputLength: 256 });
+			hash_email = CryptoJS.SHA3($scope.signup_password + $scope.signup_email, {outputLength: 256 });
+			$http.post('/signup', {'lang': $scope.lang, 'username': $scope.signup_username, 'email': $scope.signup_email, 'password_username': hash_username.toString(), 'password_email': hash_email.toString()})
 				.success( function(data) {
 					if(data == 'registered') {
 						$scope.signup_message = $translate('WELCOME.VERIFICATION_SENT') + $scope.signup_email;
 					}
-					else {
-						$scope.signup_message = $translate('WELCOME.EXISTS');
+					else if(data == 'emailExists') {
+						$scope.signup_message = $translate('WELCOME.EMAIL_EXISTS');
+					}
+					else if(data == 'usernameExists') {
+						$scope.signup_message = $translate('WELCOME.USERNAME_EXISTS');
 					}
 				});
 		} else {
@@ -19,12 +22,11 @@ function welcomeControl($rootScope, $scope, $http, $location, $translate) {
 		}
 	}
 	$scope.login = function() {
-		if($scope.login_email && $scope.login_password.length > 5) {
-			hash = CryptoJS.SHA3($scope.login_password + $scope.login_email, {outputLength: 256 });
-			$http.post('/login', {'email': $scope.login_email, 'password': hash.toString()})
+		if($scope.login_id && $scope.login_password.length > 5) {
+			hash = CryptoJS.SHA3($scope.login_password + $scope.login_id, {outputLength: 256 });
+			$http.post('/login', {'id': $scope.login_id, 'password': hash.toString()})
 				.success( function(data) {
 					if(data == 'loginSuccess') {
-						$rootScope.user = $scope.login_email;
 						$location.path('/');
 					}
 					else {
