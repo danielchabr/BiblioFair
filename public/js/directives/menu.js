02bc7350656f330c4201b3fa03dd430affedd9a0
Daniel Chabr <danielchabr@gmail.com>
2013-09-12 23:07:44 +0200

    Menu turned into directive

---
+++ public/js/directives/menu.js
@@ -0,0 +1,14 @@
+myApp.directive('menu', function() {
+	var directiveObject = {
+		restrict: 'A',
+		templateUrl: '/partials/menu.html',
+		link: function (scope, element, attrs) {
+			if(attrs.menu == 'browse') scope.browse = 'active';
+			if(attrs.menu == 'library') scope.library = 'active';
+			if(attrs.menu == 'messages') scope.messages = 'active';
+			if(attrs.menu == 'wishlist') scope.wishlist = 'active';
+			if(attrs.menu == 'account') scope.account = 'active';
+		}
+	};
+	return directiveObject;
+});
