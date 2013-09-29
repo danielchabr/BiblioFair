bc6c7525906dc611a3cc41b0927b547597b70dd1
Daniel Chabr <danielchabr@gmail.com>
2013-09-29 19:22:02 +0200

    Minor fixes and polishing

--- public/js/controllers/modalRecovery.js
+++ public/js/controllers/modalRecovery.js
@@ -2,6 +2,9 @@ var ModalRecoveryCtrl = function ($scope, $modalInstance, APIservice) {
 	$scope.remove = function () {
 		$modalInstance.close('remove');
 	};
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
 	$scope.recover = function (email) {
 		APIservice.users.update({action:'recover', email: email}, function(data){
 			$modalInstance.dismiss('cancel');
