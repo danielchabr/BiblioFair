5e0148f31840339cd9eb74b2bd2bd60565d0e3ed
Daniel Chabr <danielchabr@gmail.com>
2013-10-10 00:33:36 +0200

    Minors

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -2,6 +2,7 @@ myApp.directive('menu', function() {
 	var directiveObject = {
 		restrict: 'A',
 		templateUrl: '/partials/menu.html',
+		replace: false,
 		link: function (scope, element, attrs) {
 			if(attrs.menu == 'browse') scope.browse = 'active';
 			if(attrs.menu == 'library') scope.library = 'active';
