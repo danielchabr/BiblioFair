c15fcec47b642a8bfda3e721ba2598805fd29c39
Tadeas Petak <tadeaspetak@gmail.com>
2014-03-16 16:50:23 +0100

    Password set for dev.bibliofair.com.

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://admin:xiUk5TI6tEbv@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/'
+    db: 'mongodb://admin:Bl5vcDrcTPEJ@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/bibliofair'
 };
