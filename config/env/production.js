ed7d08bcac776e00c1af19139e8d69b5caf86c05
Tadeas Petak <tadeaspetak@gmail.com>
2014-03-16 17:25:16 +0100

    Production passport fix.

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://admin:Bl5vcDrcTPEJ@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/bibliofair'
+	db: 'mongodb://admin:Bl5vcDrcTPEJ@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/bibliofair',
+	facebook: {
+		clientID: '1481175278763417',
+		clientSecret: '63aa01bfcfbbe032d0ba21ba2db8fc2b',
+		callbackURL: 'http://localhost:8080/signin/facebook/callback'
+	},
+	google: {
+		clientID: '19373345252.apps.googleusercontent.com',
+		clientSecret: 'nY8AZuGMlZLe8JdDQM2CXTUf',
+		callbackURL: 'http://localhost:8080/signin/google/callback'
+	}
 };
