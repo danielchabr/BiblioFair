1312a3514addc7a9ab8b07c488b4c9bd92852905
Tadeas Petak <tadeaspetak@gmail.com>
2014-03-18 12:05:37 +0100

    Indentation test.

--- app/api/books.js
+++ app/api/books.js
@@ -11,7 +11,7 @@ var config = require('../../config/config'),
 				_ = require('lodash');
 
 var regex = function(s) {
-	return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+		return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 };
 
 /**
@@ -22,9 +22,9 @@ var regex = function(s) {
  */
 
 exports.count = function(done) {
-	Book.count(function(err, data) {
-		done(err, data);
-	});
+		Book.count(function(err, data) {
+				done(err, data);
+		});
 };
 
 /**
@@ -36,27 +36,27 @@ exports.count = function(done) {
  */
 
 exports.get = function(options, done) {
-	//extend defaults with the options
-	options = _.extend({
-		limit: 0,
-		offset: 0,
-		query: '',
-		fields: ''
-	}, options);
+		//extend defaults with the options
+		options = _.extend({
+				limit: 0,
+				offset: 0,
+				query: '',
+				fields: ''
+		}, options);
 
-	if(options.lat && options.lng && options.radius){
-		var re = new RegExp(regex(options.query), 'i');
-		//, num_users: {$gt: 0}
-		Book.find({$or: [{title: re}, {author: re}]}).where('loc.coordinates').near({center: [options.lng, options.lat], maxDistance: (options.radius / 111.12), spheriacal: true}).select(options.fields).skip(options.offset).limit(options.limit).populate('users', {"library": 1, _id: 0}, 'UserModel').exec(function(err, data) {
-			done(err, data);
-		});
-	} else{
-		var re = new RegExp(regex(options.query), 'i');
-		//, num_users: {$gt: 0}
-		Book.find({$or: [{title: re}, {author: re}]}).select(options.fields).skip(options.offset).limit(options.limit).populate('users', {"library": 1, _id: 0}, 'UserModel').exec(function(err, data) {
-			done(err, data);
-		});
-	}
+		if(options.lat && options.lng && options.radius){
+				var re = new RegExp(regex(options.query), 'i');
+				//, num_users: {$gt: 0}
+				Book.find({$or: [{title: re}, {author: re}]}).where('loc.coordinates').near({center: [options.lng, options.lat], maxDistance: (options.radius / 111.12), spheriacal: true}).select(options.fields).skip(options.offset).limit(options.limit).populate('users', {"library": 1, _id: 0}, 'UserModel').exec(function(err, data) {
+						done(err, data);
+				});
+		} else{
+				var re = new RegExp(regex(options.query), 'i');
+				//, num_users: {$gt: 0}
+				Book.find({$or: [{title: re}, {author: re}]}).select(options.fields).skip(options.offset).limit(options.limit).populate('users', {"library": 1, _id: 0}, 'UserModel').exec(function(err, data) {
+						done(err, data);
+				});
+		}
 };
 
 /**
@@ -68,9 +68,9 @@ exports.get = function(options, done) {
  */
 
 exports.one = function(id, done) {
-	Book.load(id, function(err, data) {
-		done(err, data);
-	});
+		Book.load(id, function(err, data) {
+				done(err, data);
+		});
 };
 
 /**
@@ -82,22 +82,22 @@ exports.one = function(id, done) {
  */
 
 exports.search = function(isbn, done) {
-	var options = require('url').parse('http://data.theeuropeanlibrary.org/opensearch/json?query=' + isbn + '&apikey=ev3fbloutqdhrqe3pidpal4bav');
-	options.rejectUnauthorized = false;
+		var options = require('url').parse('http://data.theeuropeanlibrary.org/opensearch/json?query=' + isbn + '&apikey=ev3fbloutqdhrqe3pidpal4bav');
+		options.rejectUnauthorized = false;
 
-	var data = "";
-	http.get(options, function(response) {
-		response.on('data', function(i) {
-			data += i;
+		var data = "";
+		http.get(options, function(response) {
+				response.on('data', function(i) {
+						data += i;
+				});
+				response.on('end', function() {
+						done(null, JSON.parse(data));
+				});
+				response.on('error', function(e) {
+						done(e, 'error');
+						console.log(e);
+				});
 		});
-		response.on('end', function() {
-			done(null, JSON.parse(data));
-		});
-		response.on('error', function(e) {
-			done(e, 'error');
-			console.log(e);
-		});
-	});
 }
 
 /**
@@ -105,30 +105,30 @@ exports.search = function(isbn, done) {
  */
 
 exports.report = function(user, book, done) {
-	User.findById(user, function(err, user) {
-		if(err || !user){
-			return done(err || "user.notFound");
-		}
-		else{
-			Book.findById(book, function(err, book) {
-				if(err || !book){
-					return done(err || "book.notFound");
+		User.findById(user, function(err, user) {
+				if(err || !user){
+						return done(err || "user.notFound");
 				}
 				else{
-					mg.sendText('Book report <report@bibliofair.com>',
-									config.messages.reportBookEmail,
-									messages['en'].report.subject,
-									messages['en'].report.body.replace(/\{username\}/g, user.username)
-									.replace(/\{book.title\}/, book.title)
-									.replace(/\{book.author\}/, book.author),
-									config.messages.mailgun.server,
-									function(err) {
-										done(err, 'ok');
-									});
+						Book.findById(book, function(err, book) {
+								if(err || !book){
+										return done(err || "book.notFound");
+								}
+								else{
+										mg.sendText('Book report <report@bibliofair.com>',
+														config.messages.reportBookEmail,
+														messages['en'].report.subject,
+														messages['en'].report.body.replace(/\{username\}/g, user.username)
+														.replace(/\{book.title\}/, book.title)
+														.replace(/\{book.author\}/, book.author),
+														config.messages.mailgun.server,
+														function(err) {
+																done(err, 'ok');
+														});
+								}
+						});
 				}
-			});
-		}
-	});
+		});
 };
 
 /**
@@ -136,33 +136,33 @@ exports.report = function(user, book, done) {
  */
 
 exports.request = function(from, to, book, language, done) {
-	User.findByUsername(from, function(err, from) {
-		if(err || !from){
-			return done(err || "user.notFound");
-		}
-		User.findByUsername(to, function(err, to) {
-			if(err || !to){
-				return done(err || "user.notFound");
-			}
-			Book.findById(book, function(err, book) {
-				if(err || !book){
-					return done(err || "book.notFound");
+		User.findByUsername(from, function(err, from) {
+				if(err || !from){
+						return done(err || "user.notFound");
 				}
+				User.findByUsername(to, function(err, to) {
+						if(err || !to){
+								return done(err || "user.notFound");
+						}
+						Book.findById(book, function(err, book) {
+								if(err || !book){
+										return done(err || "book.notFound");
+								}
 
-				mg.sendText(from.username + ' <' + from.username + '@bibliofair.com>',
-								to.username + ' <' + to.email + '>',
-								messages[language].request.subject,
-								(messages[language].request.body + messages[language].emailExplanation).replace(/\{from\}/g, from.username)
-								.replace(/\{to\}/g, to.username)
-								.replace(/\{book.title\}/g, book.title)
-								.replace(/\{book.author\}/g, book.author)
-								.replace(/\{book.author\}/g, book.author)
-								.replace(/\{recipient\}/g, to.username + "@bibliofair.com"),
-								config.messages.mailgun.server,
-								function(err) {
-									done(err, 'ok');
-								});
-			});
+								mg.sendText(from.username + ' <' + from.username + '@bibliofair.com>',
+												to.username + ' <' + to.email + '>',
+												messages[language].request.subject,
+												(messages[language].request.body + messages[language].emailExplanation).replace(/\{from\}/g, from.username)
+												.replace(/\{to\}/g, to.username)
+												.replace(/\{book.title\}/g, book.title)
+												.replace(/\{book.author\}/g, book.author)
+												.replace(/\{book.author\}/g, book.author)
+												.replace(/\{recipient\}/g, to.username + "@bibliofair.com"),
+												config.messages.mailgun.server,
+												function(err) {
+														done(err, 'ok');
+												});
+						});
+				});
 		});
-	});
 };
--- app/api/library.js
+++ app/api/library.js
@@ -14,9 +14,9 @@ var mongoose = require("mongoose"),
  * @returns {undefined}
  */
 exports.read = function(userId, done) {
-	User.findById(userId, 'library').populate('library.id').exec(function(err, data) {
-		done(err, data);
-	});
+		User.findById(userId, 'library').populate('library.id').exec(function(err, data) {
+				done(err, data);
+		});
 };
 
 /**
@@ -29,74 +29,74 @@ exports.read = function(userId, done) {
  */
 
 exports.add = function(userId, data, done) {
-	User.findById(userId, function(err, user) {
-		if(err || !user){
-			return done(err || 'user.notFound');
-		}
-		//verified?
-		if(!user.verified){
-			return done('user.notVerified');
-		}
-		//location
-		if(!user.isLocated()){
-			return done('user.notLocated')
-		}
-		
-		var newbook = {author: data.author, title: data.title};
-		if(data.subtitle)
-			newbook.subtitle = data.subtitle;
-		if(data.publisher)
-			newbook.publisher = data.publisher;
-		if(data.published){
-			newbook.published = new Date(data.published, 1, 1);
-		}
-		if(data.language)
-			newbook.language = data.language;
-		if(data.edition)
-			newbook.edition = data.edition;
-		if(data.volume)
-			newbook.volume = data.volume;
-		if(data.isbn)
-			newbook.isbn = data.isbn;
-		Book.findOne(newbook).exec(function(err, book) {
-			if(err){
-				done(err);
-			}
-			else if(book){
-				book.users.push(user._id);
-				book.num_users = book.num_users + 1;
-				book.loc.push({coordinates: user.loc.coordinates});
-				book.save();
-				user.library.push({id: book._id, last_updated: new Date(), actions: data.actions, note: data.note});
-				user.save();
-				done(err, book);
-			}
-			else{
-				var book = new Book();
-				book.author = newbook.author;
-				book.title = newbook.title;
-				book.subtitle = newbook.subtitle;
-				book.publisher = newbook.publisher;
-				book.published = newbook.published;
-				book.language = newbook.language;
-				book.edition = newbook.edition;
-				book.volume = newbook.volume;
-				book.isbn = newbook.isbn;
-				book.users.push(user._id);
-				book.loc.push({coordinates: user.loc.coordinates});
-				book.num_users = 1;
-				book.save(function(err, book) {
-					if(err){
-						return done(err);
-					}
-					user.library.push({id: book._id, last_updated: new Date(), actions: data.actions, note: data.note});
-					user.save(function(err, user) {
-						done(err, book);
-					});
+		User.findById(userId, function(err, user) {
+				if(err || !user){
+						return done(err || 'user.notFound');
+				}
+				//verified?
+				if(!user.verified){
+						return done('user.notVerified');
+				}
+				//location
+				if(!user.isLocated()){
+						return done('user.notLocated')
+				}
+
+				var newbook = {author: data.author, title: data.title};
+				if(data.subtitle)
+						newbook.subtitle = data.subtitle;
+				if(data.publisher)
+						newbook.publisher = data.publisher;
+				if(data.published){
+						newbook.published = new Date(data.published, 1, 1);
+				}
+				if(data.language)
+						newbook.language = data.language;
+				if(data.edition)
+						newbook.edition = data.edition;
+				if(data.volume)
+						newbook.volume = data.volume;
+				if(data.isbn)
+						newbook.isbn = data.isbn;
+				Book.findOne(newbook).exec(function(err, book) {
+						if(err){
+								done(err);
+						}
+						else if(book){
+								book.users.push(user._id);
+								book.num_users = book.num_users + 1;
+								book.loc.push({coordinates: user.loc.coordinates});
+								book.save();
+								user.library.push({id: book._id, last_updated: new Date(), actions: data.actions, note: data.note});
+								user.save();
+								done(err, book);
+						}
+						else{
+								var book = new Book();
+								book.author = newbook.author;
+								book.title = newbook.title;
+								book.subtitle = newbook.subtitle;
+								book.publisher = newbook.publisher;
+								book.published = newbook.published;
+								book.language = newbook.language;
+								book.edition = newbook.edition;
+								book.volume = newbook.volume;
+								book.isbn = newbook.isbn;
+								book.users.push(user._id);
+								book.loc.push({coordinates: user.loc.coordinates});
+								book.num_users = 1;
+								book.save(function(err, book) {
+										if(err){
+												return done(err);
+										}
+										user.library.push({id: book._id, last_updated: new Date(), actions: data.actions, note: data.note});
+										user.save(function(err, user) {
+												done(err, book);
+										});
+								});
+						}
 				});
-			}
 		});
-	});
 };
 
 /**
@@ -110,25 +110,25 @@ exports.add = function(userId, data, done) {
  */
 
 exports.update = function(userId, bookId, data, done) {
-	User.findById(userId, function(err, user) {
-		if(err || !user.library){
-			done(err);
-		}
-		else{
-			for (var i = 0; i < user.library.length; i++){
-				//different types -> == instead of ===
-				if(user.library[i].id == bookId){
-					if(data.actions)
-						user.library[i].actions = data.actions;
-					if(data.note)
-						user.library[i].note = data.note;
+		User.findById(userId, function(err, user) {
+				if(err || !user.library){
+						done(err);
 				}
-			}
-			user.save(function(err, user) {
-				done(err, user);
-			});
-		}
-	});
+				else{
+						for (var i = 0; i < user.library.length; i++){
+								//different types -> == instead of ===
+								if(user.library[i].id == bookId){
+										if(data.actions)
+												user.library[i].actions = data.actions;
+										if(data.note)
+												user.library[i].note = data.note;
+								}
+						}
+						user.save(function(err, user) {
+								done(err, user);
+						});
+				}
+		});
 };
 
 /**
@@ -141,31 +141,31 @@ exports.update = function(userId, bookId, data, done) {
  */
 
 exports.remove = function(userId, bookId, done) {
-	User.findById(userId, function(err, user) {
-		if(err || !user){
-			return done(err || "user.notFound");
-		}
-		Book.findById(bookId, function(err, book) {
-			if(err || !book.users){
-				return done(err, "book.notFound");
-			}
-			user.update({$pull: {'library': {id: bookId}}}, function(err) {
-				if(err){
-					return done(err);
+		User.findById(userId, function(err, user) {
+				if(err || !user){
+						return done(err || "user.notFound");
 				}
+				Book.findById(bookId, function(err, book) {
+						if(err || !book.users){
+								return done(err, "book.notFound");
+						}
+						user.update({$pull: {'library': {id: bookId}}}, function(err) {
+								if(err){
+										return done(err);
+								}
 
-				book.users.splice(book.users.indexOf(userId), 1);
-				book.loc.splice(book.loc.indexOf({coordinates: user.loc.coordinates}), 1);
-				book.num_users--;
-				user.save(function(err, user) {
-					if(err){
-						return done(err);
-					}
-					book.save(function(err, book) {
-						done(err, book);
-					});
+								book.users.splice(book.users.indexOf(userId), 1);
+								book.loc.splice(book.loc.indexOf({coordinates: user.loc.coordinates}), 1);
+								book.num_users--;
+								user.save(function(err, user) {
+										if(err){
+												return done(err);
+										}
+										book.save(function(err, book) {
+												done(err, book);
+										});
+								});
+						});
 				});
-			});
 		});
-	});
 };
